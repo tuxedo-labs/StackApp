0c3db3358228c9e3ef16af816c6ea99b21967373
rafia9005 <rafia9005@gmail.com>
2024-12-16 21:14:32 +0700

    enable prisma logger

--- src/application/database.ts
+++ src/application/database.ts
@@ -22,18 +22,18 @@ export const prismaClient = new PrismaClient({
   ],
 });
 
-//prismaClient.$on('query', (e) => {
-//  logger.info(e)
-//})
-//
-//prismaClient.$on('error', (e) => {
-//  logger.error(e)
-//})
-//
-//prismaClient.$on('info', (e) => {
-//  logger.info(e)
-//})
-//
-//prismaClient.$on('warn', (e) => {
-//  logger.warn(e)
-//})
+prismaClient.$on('query', (e) => {
+  logger.info(e)
+})
+
+prismaClient.$on('error', (e) => {
+  logger.error(e)
+})
+
+prismaClient.$on('info', (e) => {
+  logger.info(e)
+})
+
+prismaClient.$on('warn', (e) => {
+  logger.warn(e)
+})
